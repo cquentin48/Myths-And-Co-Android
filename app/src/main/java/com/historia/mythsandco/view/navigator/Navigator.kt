befafb1640bd499fe4b421eec0b66396f38f6432
Quentin CHAPEL <quentin.chapel@etu.univ-lyon1.fr>
2020-02-25 18:12:47 +0100

    [Feat]Navigation between activities and fragment
    Adding navigation between activities and fragment in a single class in order to properly commit changes when needed.

---
+++ app/src/main/java/com/historia/mythsandco/view/navigator/Navigator.kt
@@ -0,0 +1,24 @@
+package com.historia.mythsandco.view.navigator
+
+import android.content.Context
+import android.content.Intent
+import androidx.fragment.app.FragmentManager
+import com.historia.mythsandco.R
+import com.historia.mythsandco.view.ui.activities.ConnectionActivity
+import com.historia.mythsandco.view.ui.fragment.connectionActivity.ConnectionFragment
+
+class Navigator(private var fragmentManager : FragmentManager) {
+    //Activities
+    fun switchToConnectionActivity(context: Context){
+        context.startActivity(Intent(context,ConnectionActivity::class.java))
+    }
+
+    //Fragment
+    fun displayConnection(){
+        fragmentManager.
+            beginTransaction()
+            .replace(R.id.connection_activity_fragment_container,ConnectionFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
+    }
+}
--- app/src/main/java/com/historia/mythsandco/view/ui/activities/ConnectionActivity.kt
+++ app/src/main/java/com/historia/mythsandco/view/ui/activities/ConnectionActivity.kt
@@ -3,11 +3,14 @@ package com.historia.mythsandco.view.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.historia.mythsandco.R
+import com.historia.mythsandco.view.navigator.Navigator
 
-class MainActivity : AppCompatActivity() {
-
+class ConnectionActivity : AppCompatActivity() {
+    private lateinit var navigator : Navigator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigator = Navigator(supportFragmentManager)
+        navigator.displayConnection()
     }
 }
