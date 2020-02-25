e3307bee88faa56f13de2053a8786f2a90941d33
Quentin CHAPEL <quentin.chapel@etu.univ-lyon1.fr>
2020-02-25 18:12:47 +0100

    [Feat]Navigation between activities and fragment
    Adding navigation between activities and fragment in a single class in order to properly commit changes when needed.

--- app/src/main/java/com/historia/mythsandco/view/ui/fragment/connectionActivity/ConnectionFragment.kt
+++ app/src/main/java/com/historia/mythsandco/view/ui/fragment/connectionActivity/ConnectionFragment.kt
@@ -11,15 +11,21 @@ import kotlinx.android.synthetic.main.fragment_connection.*
 
 class ConnectionFragment : BaseFragment<ConnectionViewModel>(){
     override val viewModelClass = ConnectionViewModel::class
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connection, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         first_text_view.text = "Bonjour!"
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_connection,container!!)
+    companion object{
+        fun newInstance() = ConnectionFragment()
+    }
 }
